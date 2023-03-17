671efc51a882afb63b3f27ba346867e3acef5421
martigalan <martina.galaningelmo@usp.ceu.es>
2023-03-17 17:10:15 +0100

    changes

---
+++ src/hospital/db/pojos/Hospital.java
@@ -0,0 +1,67 @@
+package hospital.db.pojos;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Hospital implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String name;
+	private String location;
+	
+	public Hospital(String name, String location) {
+		super();
+		this.name = name;
+		this.location = location;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Hospital other = (Hospital) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Hospital [id=" + id + ", name=" + name + ", location=" + location + "]";
+	}
+	
+	
+	
+}
