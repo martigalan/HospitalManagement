e541db6818997e118866a7741c2efa41729754f6
Blanca <blancapueche@gmail.com>
2023-04-09 19:41:41 +0200

    db uploaded to be able to create Connections

--- src/hospital/jdbc/JDBCPatientManager.java
+++ src/hospital/jdbc/JDBCPatientManager.java
@@ -3,10 +3,24 @@ import hospital.db.ifaces.PatientManager;
 import hospital.db.pojos.Patient;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.List;
 
 public class JDBCPatientManager implements PatientManager{
 
+	Connection c;
+	public JDBCPatientManager() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:./db/HospitalManagement.db");
+			c.createStatement().execute("PRAGMA foreign_keys=ON");
+			System.out.println("Database connection opened.");
+		} catch (Exception e) {
+			System.out.println("Database access error");
+			e.printStackTrace();
+		}
+	}
+	
 	@Override
 	public void updatePatient(String name, String surname) {
 		// TODO Auto-generated method stub
