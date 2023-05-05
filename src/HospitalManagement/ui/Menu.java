d77c0964fe8ad71e588fe2beb17c44071ac4b32d
martigalan <martina.galaningelmo@usp.ceu.es>
2023-05-05 17:31:06 +0200

    New XML methods

--- src/HospitalManagement/ui/Menu.java
+++ src/HospitalManagement/ui/Menu.java
@@ -101,26 +101,27 @@ public class Menu {
 		String name = sc.nextLine();
 		System.out.println("Surname:");
 		String surname = sc.nextLine();
+		Date dobDate = null;
 		try {
 			System.out.println("Date of birth (yyyy-MM-dd):");
 			String dob = sc.nextLine();
 			LocalDate dobLocalDate = LocalDate.parse(dob); // java.time.LocalDate
-			Date dobDate = Date.valueOf(dobLocalDate);
+			dobDate = Date.valueOf(dobLocalDate);
 		} catch (DateTimeParseException d) {
 			System.out.println("Put the date of birth in the correct format: (yyyy-MM-dd)");
 			String dob = sc.nextLine();
 			LocalDate dobLocalDate = LocalDate.parse(dob); // java.time.LocalDate
-			Date dobDate = Date.valueOf(dobLocalDate);
+			dobDate = Date.valueOf(dobLocalDate);
 		}
 		// System.out.println("Photo:");
 		// byte[] photo = sc.nextByte();
-		System.out.print("Type the file name as it appears in folder /photos, including extension: ");
+		/*System.out.print("Type the file name as it appears in folder /photos, including extension: ");
 		String fileName = sc.nextLine();
 		File photos = new File("./photos/" + fileName);
 		InputStream streamBlob = new FileInputStream(photos);
 		byte[] photo = new byte[streamBlob.available()];
 		streamBlob.readAllBytes();
-		streamBlob.close();
+		streamBlob.close();*/
 		// TODO add photo in SQLinsert
 		/*List<Hospital> possibleH = hospitalM.searchByName("Hospital Universitario Fundación Jimenez Díaz");
 		Integer hospId = null;
@@ -131,7 +132,7 @@ public class Menu {
 		Hospital mainHospital = hospitalM.getHospital(hospId);*/
 		
 		Hospital mainHospital = hospitalM.search1ByName("Hospital Universitario Fundación Jimenez Díaz");
-		
+		byte[] photo = null;
 
 		Patient p = new Patient(name, surname, dobDate, mainHospital, photo);
 		patientM.insertPatient(p);
